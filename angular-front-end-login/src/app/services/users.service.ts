747893ba7a0ebb0e4eb2d0de4e8256ff130fbc25
Gscavo <gsscavo@gmail.com>
2023-08-07 21:49:22 -0300

    Service made

--- angular-front-end-login/src/app/services/users.service.ts
+++ angular-front-end-login/src/app/services/users.service.ts
@@ -2,19 +2,54 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { UserModel } from '../model/UserModel';
 import { environment } from '../../environments/environment.development';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-
+  // urls
+  private ws_url = environment.WS_ENDPOINT;
   private api_url = environment.API_PATH;
+  
+  //real-time-web-socket
+  private socket$!: WebSocket;
+  messageReceived: Subject<string> = new Subject<string>();
 
   constructor(private httpClient: HttpClient) { }
 
-  getUsers(): Observable<UserModel[]> {
-    return this.httpClient.get<UserModel[]>(this.api_url+'getData/');
+  connectToWebSocket(): void {
+    this.socket$ = new WebSocket(this.ws_url);
+
+    this.socket$.onopen = () => {
+      console.log('WebSocket connection established.');
+    }
+
+    this.socket$.onmessage = (event) => {
+      const message = event.data;
+      console.log('Received message: ', message);
+      this.messageReceived.next(message);
+    }
+
+    this.socket$.onclose = (event) => {
+      console.log('Closed Connection! ', event);
+    }
+
+    this.socket$.onerror = (error) => {
+      console.log('WebSocket Error: ', error);
+    }
+  }
+
+  sendMessage(message: string): void {
+    this.socket$.send(message);
+  }
+
+  closeConnection(): void {
+    this.socket$.close();
+  }
+
+  getUsers(): Observable<UserModel> {
+    return this.httpClient.get<UserModel>(this.api_url+'getData/');
   }
 
   getUserById(id: String): Observable<UserModel> {
