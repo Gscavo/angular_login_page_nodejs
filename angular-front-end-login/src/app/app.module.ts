831f589470e7a1839e17c5b8f936abb348c97b7f
Gscavo <gsscavo@gmail.com>
2023-08-08 09:23:34 -0300

    added footer component

--- angular-front-end-login/src/app/app.module.ts
+++ angular-front-end-login/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { UsersListComponent } from './pages/users-list/users-list.component';
 import { BackButtonComponent } from './components/back-button/back-button.component';
 import { ButtonsComponent } from './components/buttons/buttons.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { RouterModule } from '@angular/router';
     UsersListComponent,
     BackButtonComponent,
     ButtonsComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
