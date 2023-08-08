6a97527ad47a3d039959893d80ebbfed5a1e2cb2
Gscavo <gsscavo@gmail.com>
2023-08-07 21:49:16 -0300

    list view.ts

--- angular-front-end-login/src/app/components/list-view/list-view.component.ts
+++ angular-front-end-login/src/app/components/list-view/list-view.component.ts
@@ -1,40 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { UserModel } from 'src/app/model/UserModel';
-import { UsersService } from 'src/app/services/users.service';
+import { Component, OnInit } from "@angular/core";
+import { Observable, catchError, map, tap } from "rxjs";
+import { UserModel } from "src/app/model/UserModel";
+import { UsersService } from "src/app/services/users.service";
 
 @Component({
-  selector: 'app-list-view',
-  templateUrl: './list-view.component.html',
-  styleUrls: ['./list-view.component.css']
+  selector: "app-list-view",
+  templateUrl: "./list-view.component.html",
+  styleUrls: ["./list-view.component.css"],
 })
 export class ListViewComponent implements OnInit {
 
-  usersList: UserModel[] = [];
-  refreshUsers: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  receivedMessages: string[] = [];
 
   constructor(private service: UsersService) {  }
 
   ngOnInit(): void {
-      
+    this.service.connectToWebSocket();
+    this.service.messageReceived.subscribe((message: string) => {
+      this.receivedMessages.push(message);
+    });
   }
-
-  getUserData() {
-    this.service.getUsers().subscribe( (data) => {
-      data.forEach(el => {
-        let userData: UserModel = {
-          firstName: el.firstName,
-          lastName: el.lastName,
-          email: el.email,
-          age: el.age,
-          sex: el.sex,
-        }
-        if (this.usersList.indexOf(userData) === -1) {
-          console.log(userData);
-          this.usersList.push(userData);
-          console.log(this.usersList);
-        }
-      })
-    })
+  
+  sendMessage(): void {
+    const message = '{"Hello": "World"}';
+    this.service.sendMessage(message);
   }
 }
